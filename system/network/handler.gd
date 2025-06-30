28af3a38df267b7be451fef5a8800ad577f9a6c5
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2025-06-30 18:12:55 -0300

    Handler e ServerScene

--- system/network/handler.gd
+++ system/network/handler.gd
@@ -1,2 +1,41 @@
 class_name Handler
 extends RefCounted
+
+
+var _packets: Dictionary[int, Object] = {}
+
+
+func _init(packets: Array[Object] = []) -> void:
+  for packet in packets:
+    if packet.has_method("handle"):
+      _packets[packet.id] = packet
+
+
+func handle(scene: SceneTree, packet_data: PackedByteArray, peer_id: int = -1) -> void:
+  var data: Array = bytes_to_var(packet_data)
+  if typeof(data) != TYPE_ARRAY or data.size() < 2:
+    return
+  
+  var packet_id = data[0]
+  var arguments = data[1]
+
+  if typeof(packet_id) != TYPE_INT or typeof(arguments) != TYPE_ARRAY:
+    return
+  
+  if not _packets.has(packet_id):
+    return
+  
+  var packet = _packets[packet_id]
+  if not packet.has_method("handle"):
+    return
+  
+  arguments.append(scene)
+
+  if peer_id != -1:
+    arguments.append(peer_id)
+
+  var expected_args = packet.get_method_argument_count("handle")
+  if expected_args != arguments.size():
+    return
+  
+  packet.callv("handle", arguments)
--- system/scenes/server.gd
+++ system/scenes/server.gd
@@ -1,2 +1,35 @@
 class_name ServerScene
 extends Control
+
+
+var _server: Server
+var _handler: Handler
+
+
+func _init() -> void:
+  _server = Network.server
+  _handler = Handler.new([])
+
+
+func _ready() -> void:
+  _server.start()
+  
+  _server.peer_connected.connect(_peer_connected)
+  _server.peer_disconnected.connect(_peer_disconnected)
+  _server.packet_received.connect(_packet_received)
+
+
+func _process(_delta: float) -> void:
+  _server.update()
+
+
+func _peer_connected(peer_id: int) -> void:
+  print("Cliente conectado: ", str(peer_id))
+
+
+func _peer_disconnected(peer_id: int) -> void:
+  print("Cliente desconectado: ", str(peer_id))
+
+
+func _packet_received(peer_id: int, packet_data: PackedByteArray) -> void:
+  _handler.handle(get_tree(), packet_data, peer_id)
