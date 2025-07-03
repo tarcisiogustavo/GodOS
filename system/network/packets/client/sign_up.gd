c4563c0175dfa99486903dce0e8ddc0892fa9b4a
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2025-07-03 15:25:22 -0300

    Update sign_up.gd

--- system/network/packets/client/sign_up.gd
+++ system/network/packets/client/sign_up.gd
@@ -6,15 +6,18 @@ var packet_id: int = Packets.SIGN_UP
 
 
 func handle(success: String, error: Array, scene: SceneTree) -> void:
-	if not error.is_empty():
-		Notification.show(error)
-		return
-
 	var sign_up_interface: SignUpInterface = scene.root.get_node("Client/MenuCanvas/SignUp")
 	var sign_in_interface: SignInInterface = scene.root.get_node("Client/MenuCanvas/SignIn")
 
+	if not error.is_empty():
+		sign_up_interface.reset_form()
+
+		Notification.show(error)
+		return
+
 	sign_up_interface.hide()
 	sign_up_interface.reset_form()
+
 	sign_in_interface.show()
 
 	Notification.show([success])
